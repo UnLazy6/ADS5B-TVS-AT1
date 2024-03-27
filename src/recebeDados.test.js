5d0775796bb08d865ed71d4486975ed6b0023f4c
UnLazy6 <caiovictortv6@gmail.com>
2024-03-27 19:56:36 -0300

    aula benones

--- src/recebeDados.test.js
+++ src/recebeDados.test.js
@@ -2,9 +2,9 @@
 const dadosEstudante = require('./dadosEstudante');
 
 describe('Teste da função recebeDados', () => {
-  test('Verifica se a função processa corretamente o nome "Mellinda Gates" e o RA "228697"', () => {
-    const nome = "Mellinda Gates";
-    const RA = "228697";
+  test('Verifica se a função processa corretamente o nome "Caio Victor Dantas Oliveira" e o RA "181960"', () => {
+    const nome = "Caio Victor Dantas Oliveira";
+    const RA = "181960";
     const esperado = `Dados recebidos - Nome: ${nome}, RA: ${RA}`;
 
     expect(dadosEstudante(nome, RA)).toBe(esperado);
